79684b0fa5be427c7457e4c0e8c48314b4979e4a
tallman <vedunaik@gmail.com>
2024-06-10 14:56:06 +0530

    changed fab-mobile & desktop svg

--- app/data/QuestionsData..js
+++ app/data/QuestionsData..js
@@ -42,46 +42,6 @@ export const questions = [
             image: eye,
             text: '100 Views'
         }
-    },
-    {
-        id: 3,
-        date: 'Posted on 28 jun 2023',
-        category: {
-            image: Rocket,
-            text: 'Startip'
-        },
-        paragraph:
-            'A travel startup wants Amazon to pre-install their personal travel agent bot on existing Amazon Echos. What is the value of the partnership to the travel startup? A travel startup wants Amazon to pre-install their personal travel agent bot on existing Amazon Echos. What is the value of the partnership to the travel startup?',
-        role: ['Design', 'UX'],
-        complexity: 'Easy',
-        comment: {
-            image: comment,
-            text: '23 Answers'
-        },
-        Views: {
-            image: eye,
-            text: '100 Views'
-        }
-    },
-    {
-        id: 4,
-        date: 'Posted on 28 jun 2023',
-        category: {
-            image: Rocket,
-            text: 'Startip'
-        },
-        paragraph:
-            'A travel startup wants Amazon to pre-install their personal travel agent bot on existing Amazon Echos. What is the value of the partnership to the travel startup? A travel startup wants Amazon to pre-install their personal travel agent bot on existing Amazon Echos. What is the value of the partnership to the travel startup?',
-        role: ['Design', 'UX'],
-        complexity: 'Easy',
-        comment: {
-            image: comment,
-            text: '23 Answers'
-        },
-        Views: {
-            image: eye,
-            text: '100 Views'
-        }
     }
 ];
 
