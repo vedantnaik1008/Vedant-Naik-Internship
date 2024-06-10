447682a099715adac9eca5d8ac048c3598c77506
tallman <vedunaik@gmail.com>
2024-06-10 21:41:01 +0530

    removed svg and + images

--- app/components/Answers.jsx
+++ app/components/Answers.jsx
@@ -11,6 +11,8 @@ import userProfile from '@/public/user-profile.png';
 import likesImg from '@/public/Likes-img.png'
 import message from '@/public/message.png';
 import { MyContext } from '../Provider/contextProvider';
+import downArrrow from '@/public/svg/down-arrow.svg'
+import edit from '@/public/svg/edit.svg';
 
 const Answers = () => {
     const { click, auth } = useContext(MyContext);
@@ -22,156 +24,156 @@ const Answers = () => {
                   <p className={`${poppins.className}`}>Back to Questions</p>
               </div>
 
-                <div className="answer-container-flex">
-                    <div className={`section-question-frame ${click ? 'question-frame-margin' : ''}`}>
-                  <div className='question-first-row'>
-                      <div className={`question-row ${inter.className}`}>
-                          <button>Design</button>
-                          <button>Technology</button>
-                      </div>
-                      <div className='category'>
-                          <Image
-                              src={Rocket}
-                              width={50}
-                              height={50}
-                              alt='rocket-icon'
-                          />
-                          <p className={poppins.className}>Startup</p>
-                      </div>
-                  </div>
-                  <div className={`question-second-row ${poppins.className}`}>
-                      <p>
-                          A travel startup wants Amazon to pre-install their
-                          personal travel agent bot on existing Amazon Echos.
-                          What is the value of the partnership to the travel
-                          startup?{' '}
-                      </p>
-                      <p>
-                          Lorem ipsum dolor sit amet consectetur. Orci elementum
-                          aliquet nec viverra tincidunt ? Amet ullamcorper velit
-                          tristique scelerisque donec sed viverra arcu. Amet
-                          arcu vitae sit scelerisque ultrices magna cursus se?{' '}
-                      </p>
-                  </div>
-                  <div className={`question-third-row ${poppins.className}`}>
-                      <div className='flex'>
-                          <Image
-                              src={eye}
-                              width={23.82}
-                              height={15}
-                              alt='eye'
-                          />{' '}
-                          <p>100 Views</p>
-                      </div>
-                      <div className='flex'>
-                          <Image
-                              src={information}
-                              width={22}
-                              height={22}
-                              alt='information'
-                          />{' '}
-                          <p>How should you word your answer?</p>
-                      </div>
-                  </div>
-              </div>
-
-              <div className={`answers-sortby ${poppins.className} ${click ? 'answer-sortby-order' : ''}`}>
-                  <p>Answers (23)</p>
-                  <label htmlFor=''>
-                      <p className={inter.className}>Sort By:</p>
-                      <svg
-                          className='down-arrow'
-                          width='21'
-                          height='9'
-                          viewBox='0 0 21 9'
-                          fill='none'
-                          xmlns='http://www.w3.org/2000/svg'>
-                          <path
-                              d='M0.919067 0.231445L10.9596 8.76876L21 0.231445H0.919067Z'
-                              fill='#2A2A2A'
-                          />
-                      </svg>
-
-                      <select className={poppins.className}>
-                          <option value='popular'>Popular</option>
-                      </select>
-                  </label>
-              </div>
-
-              <div className='section-answer-frame'>
-                  <div className={`user-answer-frame ${poppins.className}`}>
-                      <div className='user'>
-                          <div className='user-profile'>
+              <div className='answer-container-flex'>
+                  <div
+                      className={`section-question-frame ${
+                          click ? 'question-frame-margin' : ''
+                      }`}>
+                      <div className='question-first-row'>
+                          <div className={`question-row ${inter.className}`}>
+                              <button>Design</button>
+                              <button>Technology</button>
+                          </div>
+                          <div className='category'>
                               <Image
-                                  src={userProfile}
-                                  alt='user-profile'
-                                  width={60}
-                                  height={60}
+                                  src={Rocket}
+                                  width={50}
+                                  height={50}
+                                  alt='rocket-icon'
                               />
-                              <div className='user-info'>
-                                  <h3>
-                                      Neha Bhat <span>{'(You)'}</span>
-                                  </h3>
-                                  <p>Jun 27, 2023</p>
+                              <p className={poppins.className}>Startup</p>
+                          </div>
+                      </div>
+                      <div
+                          className={`question-second-row ${poppins.className}`}>
+                          <p>
+                              A travel startup wants Amazon to pre-install their
+                              personal travel agent bot on existing Amazon
+                              Echos. What is the value of the partnership to the
+                              travel startup?{' '}
+                          </p>
+                          <p>
+                              Lorem ipsum dolor sit amet consectetur. Orci
+                              elementum aliquet nec viverra tincidunt ? Amet
+                              ullamcorper velit tristique scelerisque donec sed
+                              viverra arcu. Amet arcu vitae sit scelerisque
+                              ultrices magna cursus se?{' '}
+                          </p>
+                      </div>
+                      <div
+                          className={`question-third-row ${poppins.className}`}>
+                          <div className='flex'>
+                              <Image
+                                  src={eye}
+                                  width={23.82}
+                                  height={15}
+                                  alt='eye'
+                              />{' '}
+                              <p>100 Views</p>
+                          </div>
+                          <div className='flex'>
+                              <Image
+                                  src={information}
+                                  width={22}
+                                  height={22}
+                                  alt='information'
+                              />{' '}
+                              <p>How should you word your answer?</p>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div
+                      className={`answers-sortby ${poppins.className} ${
+                          click ? 'answer-sortby-order' : ''
+                      }`}>
+                      <p>Answers (23)</p>
+                      <label htmlFor=''>
+                          <p className={inter.className}>Sort By:</p>
+                          <Image
+                              className='down-arrow'
+                              src={downArrrow}
+                              alt='svg'
+                              width={21}
+                              height={9}
+                          />
+                          <select className={poppins.className}>
+                              <option value='popular'>Popular</option>
+                          </select>
+                      </label>
+                  </div>
+
+                  <div className='section-answer-frame'>
+                      <div className={`user-answer-frame ${poppins.className}`}>
+                          <div className='user'>
+                              <div className='user-profile'>
+                                  <Image
+                                      src={userProfile}
+                                      alt='user-profile'
+                                      width={60}
+                                      height={60}
+                                  />
+                                  <div className='user-info'>
+                                      <h3>
+                                          Neha Bhat <span>{'(You)'}</span>
+                                      </h3>
+                                      <p>Jun 27, 2023</p>
+                                  </div>
+                              </div>
+                              <div className='edit'>
+                                  <span>
+                                      <Image
+                                          src={edit}
+                                          alt='svg'
+                                          width={20}
+                                          height={20}
+                                      />
+                                  </span>
+                                  <p>Edit</p>
                               </div>
                           </div>
-                          <div className='edit'>
+                          <p className='user-answer'>
+                              Lorem ipsum dolor sit amet consectetur. Elit et ut
+                              at vestibulum enim ornare feugi vitae. Eget proin
+                              aliquam blandit eget vitae erat fermentum lacus.
+                              Dignissim done mi vel fermentum. Id ultrices risus
+                              sit pel sit elit morbi. Mi sed mauris aenean odio
+                              egestas ullamcorper. Dignissim in vel fusce id.
+                              Sit blandit diam ridiculus ipsum{' '}
                               <span>
-                                  <svg
-                                      width='20'
-                                      height='20'
-                                      viewBox='0 0 20 20'
-                                      fill='none'
-                                      xmlns='http://www.w3.org/2000/svg'>
-                                      <path
-                                          d='M19.2 18.1H0.8C0.3575 18.1 0 18.4575 0 18.9V19.8C0 19.91 0.09 20 0.2 20H19.8C19.91 20 20 19.91 20 19.8V18.9C20 18.4575 19.6425 18.1 19.2 18.1ZM3.6425 16C3.6925 16 3.7425 15.995 3.7925 15.9875L7.9975 15.25C8.0475 15.24 8.095 15.2175 8.13 15.18L18.7275 4.5825C18.7507 4.55937 18.7691 4.5319 18.7816 4.50166C18.7942 4.47141 18.8006 4.43899 18.8006 4.40625C18.8006 4.37351 18.7942 4.34109 18.7816 4.31084C18.7691 4.2806 18.7507 4.25313 18.7275 4.23L14.5725 0.0725C14.525 0.025 14.4625 0 14.395 0C14.3275 0 14.265 0.025 14.2175 0.0725L3.62 10.67C3.5825 10.7075 3.56 10.7525 3.55 10.8025L2.8125 15.0075C2.78818 15.1414 2.79687 15.2793 2.83782 15.4091C2.87877 15.5389 2.95074 15.6568 3.0475 15.7525C3.2125 15.9125 3.42 16 3.6425 16Z'
-                                          fill='#0F0F0F'
-                                      />
-                                  </svg>
-                              </span>
-                              <p>Edit</p>
-                          </div>
-                      </div>
-                      <p className='user-answer'>
-                          Lorem ipsum dolor sit amet consectetur. Elit et ut at
-                          vestibulum enim ornare feugi vitae. Eget proin aliquam
-                          blandit eget vitae erat fermentum lacus. Dignissim
-                          done mi vel fermentum. Id ultrices risus sit pel sit
-                          elit morbi. Mi sed mauris aenean odio egestas
-                          ullamcorper. Dignissim in vel fusce id. Sit blandit
-                          diam ridiculus ipsum{' '}
-                          <span>
-                              interdum ut velit quam. Bibendum amet mi....
-                          </span>{' '}
-                          <span>Show more</span>
-                      </p>
-                      <div className='comment-frame'>
-                          <div className='upvote'>
-                              <Image
-                                  src={likesImg}
-                                  alt='like-emoji'
-                                  width={25}
-                                  height={25}
-                              />
-                              <p>Like</p>
-                          </div>
-                          <div className='comment-box'>
-                              <Image
-                                  src={message}
-                                  alt='message'
-                                  width={25}
-                                  height={25}
-                              />
-                              <input type='text' placeholder='Add a comment' />
-                              <button className={poppins.className}>
-                                  Post
-                              </button>
+                                  interdum ut velit quam. Bibendum amet mi....
+                              </span>{' '}
+                              <span>Show more</span>
+                          </p>
+                          <div className='comment-frame'>
+                              <div className='upvote'>
+                                  <Image
+                                      src={likesImg}
+                                      alt='like-emoji'
+                                      width={25}
+                                      height={25}
+                                  />
+                                  <p>Like</p>
+                              </div>
+                              <div className='comment-box'>
+                                  <Image
+                                      src={message}
+                                      alt='message'
+                                      width={25}
+                                      height={25}
+                                  />
+                                  <input
+                                      type='text'
+                                      placeholder='Add a comment'
+                                  />
+                                  <button className={poppins.className}>
+                                      Post
+                                  </button>
+                              </div>
                           </div>
                       </div>
                   </div>
               </div>
-                </div>
-              
           </div>
       </section>
   );
--- app/components/ReportIssue.jsx
+++ app/components/ReportIssue.jsx
@@ -2,6 +2,9 @@
 import React, { useContext, useState } from 'react'
 import { poppins } from '../page';
 import { MyContext } from '../Provider/contextProvider';
+import redStar from '@/public/svg/red-star.svg';
+import attach from '@/public/svg/attach.svg';
+import Image from 'next/image';
 const ReportIssue = () => {
     const { click, setClick, optionClick, auth, setAuth, setOptionClick } =
         useContext(MyContext);
@@ -103,20 +106,12 @@ const disabledLoggedOutState = !auth.logged
                   <label htmlFor=''>
                       <p>Describe the issue in detail</p>{' '}
                       <span>
-                          <svg
-                              width='10'
-                              height='10'
-                              viewBox='0 0 10 10'
-                              fill='none'
-                              xmlns='http://www.w3.org/2000/svg'>
-                              <path
-                                  d='M5.00013 1V9M8.46413 3L1.53613 7M1.53613 3L8.46413 7'
-                                  stroke='#FD443A'
-                                  stroke-width='1.33333'
-                                  stroke-linecap='round'
-                                  stroke-linejoin='round'
-                              />
-                          </svg>
+                          <Image
+                              src={redStar}
+                              width={10}
+                              height={10}
+                              alt='svg'
+                          />
                       </span>
                   </label>
                   <textarea
@@ -128,17 +123,12 @@ const disabledLoggedOutState = !auth.logged
                   />
                   <button className='attach'>
                       <span>
-                          <svg
-                              width='10'
-                              height='20'
-                              viewBox='0 0 10 20'
-                              fill='none'
-                              xmlns='http://www.w3.org/2000/svg'>
-                              <path
-                                  d='M8.74968 5.62309V14.4398C8.74968 16.1814 7.47468 17.7314 5.74134 17.8981C5.2783 17.9444 4.8107 17.893 4.36874 17.7473C3.92678 17.6016 3.5203 17.3648 3.17554 17.0523C2.83078 16.7397 2.55542 16.3583 2.36723 15.9327C2.17904 15.5071 2.08222 15.0468 2.08301 14.5814V4.28142C2.08301 3.18975 2.86634 2.19809 3.94968 2.08975C4.2402 2.05937 4.53387 2.09043 4.81162 2.18091C5.08937 2.27138 5.34499 2.41926 5.56188 2.61493C5.77877 2.81061 5.95209 3.0497 6.07058 3.3167C6.18907 3.5837 6.25009 3.87264 6.24968 4.16475V12.9148C6.24968 13.3731 5.87468 13.7481 5.41634 13.7481C4.95801 13.7481 4.58301 13.3731 4.58301 12.9148V5.62309C4.58301 5.28142 4.29968 4.99809 3.95801 4.99809C3.61634 4.99809 3.33301 5.28142 3.33301 5.62309V12.7981C3.33301 13.8898 4.11634 14.8814 5.19968 14.9898C5.4902 15.0201 5.78387 14.9891 6.06162 14.8986C6.33937 14.8081 6.59499 14.6602 6.81188 14.4646C7.02877 14.2689 7.20209 14.0298 7.32058 13.7628C7.43907 13.4958 7.50009 13.2069 7.49968 12.9148V4.30642C7.49968 2.56475 6.22468 1.01475 4.49134 0.848085C4.02843 0.802477 3.56109 0.854309 3.11942 1.00024C2.67775 1.14618 2.27153 1.38298 1.92691 1.69541C1.5823 2.00784 1.30693 2.38897 1.11852 2.81427C0.93012 3.23956 0.832861 3.6996 0.833008 4.16475V14.3898C0.833008 16.7814 2.58301 18.9231 4.96634 19.1481C7.70801 19.3981 9.99968 17.2648 9.99968 14.5814V5.62309C9.99968 5.28142 9.71634 4.99809 9.37468 4.99809C9.03301 4.99809 8.74968 5.28142 8.74968 5.62309Z'
-                                  fill='#333333'
-                              />
-                          </svg>
+                          <Image
+                              src={attach}
+                              width={10}
+                              height={20}
+                              alt='svg'
+                          />
                       </span>
                       <p>Attach</p>
                   </button>
@@ -149,20 +139,12 @@ const disabledLoggedOutState = !auth.logged
                       <label htmlFor=''>
                           <p>Enter your email to receive an update</p>
                           <span>
-                              <svg
-                                  width='10'
-                                  height='10'
-                                  viewBox='0 0 10 10'
-                                  fill='none'
-                                  xmlns='http://www.w3.org/2000/svg'>
-                                  <path
-                                      d='M5.00013 1V9M8.46413 3L1.53613 7M1.53613 3L8.46413 7'
-                                      stroke='#FD443A'
-                                      stroke-width='1.33333'
-                                      stroke-linecap='round'
-                                      stroke-linejoin='round'
-                                  />
-                              </svg>
+                              <Image
+                                  src={redStar}
+                                  width={10}
+                                  height={10}
+                                  alt='svg'
+                              />
                           </span>
                       </label>
                       <input
